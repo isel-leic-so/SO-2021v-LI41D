88ebae2e9bc32c29535cf1baa2d96ce2b92bda20
jmartins3 <jmartins@isel.pt>
2021-06-23 19:30:45 +0100

    code commented for the new version of the libuv echo server

--- aula-06-23/echo_libuv/echo_uv_srv.c
+++ aula-06-23/echo_libuv/echo_uv_srv.c
@@ -1,3 +1,11 @@
+/**
+ * this is a version of the worker server that illustrate
+ * the use of the thread pool for leaving the I/O pool time 
+ * to maximize the I/O processing
+ * where the request processing consumes CPU time (wich is really not the case
+ * of the echo server, of course :) )
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,20 +17,35 @@
  
 #define DEFAULT_BACKLOG 128
 
+
+// globals
 static uv_loop_t loop;
 static uv_tcp_t server;	
-    
+
+// just for stat purposes
+static int nreads =0;
 
 
+
+/**
+  A wrapper that allow the write completed callback
+  to free the associated buffer
+  In this version is not used, since the buffer is 
+  associated to uv_write_t via is "data" field 
+*/
 typedef struct {
     uv_write_t req;
     uv_buf_t buf;
 } write_req_t;
 
+
 void free_write_req(uv_write_t *req) {
+	// the old way
     //write_req_t *wr = (write_req_t*) req;
     //free(wr->buf.base);
     
+    // the new version that use the "data field" of uv_write_t request
+    // the allow the free of the associated buffer
     free(req->data);
     free(req);
 }
@@ -35,9 +58,12 @@ void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
 }
 
 void on_close(uv_handle_t* handle) {
+	// here the loop associated handle is freed
     free(handle);
 }
 
+// the write completion callback
+//
 void echo_write_completed(uv_write_t *req, int status) {
     if (status) {
         fprintf(stderr, "Write error %s\n", uv_strerror(status));
@@ -45,9 +71,10 @@ void echo_write_completed(uv_write_t *req, int status) {
     free_write_req(req);
 }
 
-static int nreads =0;
 
-
+// the wrapper to mantain the state
+// associated to the request being processed
+// on libuv thread pool
 typedef struct uv_echo_work {
 	uv_work_t req;
 	uv_stream_t *client;
@@ -55,23 +82,36 @@ typedef struct uv_echo_work {
 } uv_echo_work_t;
 
 
+/**
+ *  the actual worker
+ *  Here, since we have a simple echo server
+ *  the "work" is simulated by a sleep
+*/
 void do_work(uv_work_t *w) {
 	sleep(1);
 	printf("do work in thread %ld\n", uv_thread_self());
 }
 
+
+/**
+ * The after callback that executes in the I/O loop thread
+ * Has is required for use of the associated handles
+ * Here we send the response to our client
+ */
 void send_resp(uv_work_t *w, int status) {
 	uv_echo_work_t *wreq = (uv_echo_work_t *) w;
 	
 	printf("send_resp in thread %ld\n", uv_thread_self());
-	uv_write_t *req = malloc(sizeof(uv_write_t));
-        
-	req->data = wreq->buf.base;
-	 
+	
+	uv_write_t *req = malloc(sizeof(uv_write_t));    
+	req->data = wreq->buf.base; 
 	uv_write((uv_write_t*) req, wreq->client, &wreq->buf, 1, echo_write_completed);
 }
 
 
+/**
+ * Prepare and submit the work to the libuv thread pool
+ */
 static void dispatch_work(uv_stream_t *client, ssize_t nread, void *data) {
 	uv_echo_work_t *req = malloc(sizeof(uv_echo_work_t));
 	req->client = client;
@@ -80,6 +120,10 @@ static void dispatch_work(uv_stream_t *client, ssize_t nread, void *data) {
 	uv_queue_work(&loop, (uv_work_t *) req, do_work, send_resp);
 }
 
+
+/**
+ * the callback for processed of each buffer with data received from client
+ */
 void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
 	//printf("echo_read %d\n", nreads);
 
@@ -104,6 +148,10 @@ void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
     free(buf->base);
 }
 
+
+/**
+ * The caclback for resolve a new client connection
+ */
 void on_new_connection(uv_stream_t *server, int status) {
     if (status < 0) {
         fprintf(stderr, "New connection error %s\n", uv_strerror(status));
